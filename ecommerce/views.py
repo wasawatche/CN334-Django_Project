fd7f8a63d6ec7f152e45fccd65a7879ee112bfde
Muuwu <wasawat.csm@gmail.com>
2024-01-22 15:30:52 +0700

    Adding new ecommerce app and item page

---
+++ ecommerce/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def ecommerce_index_view(request):
+    '''This function render index page of ecommerce views'''
+
+    return HttpResponse('Welcome to 6410742735 Wasawat Cheepsamut views!')
+
+def item_view(request, item_id):
+
+    context_data = {
+        "item_id" : item_id
+    }
+
+    return render(request, 'index.html', context = context_data)
